3795c7877317fb76c1c3578833551416b516e647
Lypeix <patrykkudla1010@gmail.com>
2026-07-13 15:49:07 +0200

    Add files via upload

---
+++ storage.py
@@ -0,0 +1,39 @@
+from models import Book
+import json
+
+def save_books(books):
+    data = []
+
+    for book in books:
+        data.append({
+            "name": book.name,
+            "author": book.author,
+            "price": book.price,
+            "genre": book.genre,
+            "date": book.date
+            })
+
+    with open("books.json", "w") as file:
+        json.dump(data, file, indent=4)
+
+def load_books():
+    try:
+        with open("books.json", "r") as file:
+            data = json.load(file)
+    except (FileNotFoundError, json.JSONDecodeError):
+        return []
+
+    books = []
+
+    for value in data:
+        book = Book({
+            value['name'],
+            value['author'],
+            value['price'],
+            value['genre'],
+            value['date']
+        })
+
+        books.append(book)
+
+    return books
