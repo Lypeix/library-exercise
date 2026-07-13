b74416e9939c0d61461af767ba1127cbbf00bf2f
Lypeix <patrykkudla1010@gmail.com>
2026-07-13 15:49:39 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from models import Book, Library
-
+from storage import save_books, load_books
 
 book1 = Book("Glory of Rome",
              "Edward",
@@ -9,8 +9,11 @@ book1 = Book("Glory of Rome",
              )
 
 library = Library()
+library.books = load_books()
 
 library.book_add(book1)
+save_books(library.books)
+
 
 for book in library.books:
     print(f"Name: {book.name} - Author: {book.author} - Price: {book.price} - Genre: {book.genre} - Added on: {book.date}")
